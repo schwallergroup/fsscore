22e7a99ca576c5f687582956cf13fd760153bbb3
rneeser <rebecca.neeser@epfl.ch>
2023-07-13 18:16:45 +0200

    add: paths to training files

--- src/intuitive_sc/utils/paths.py
+++ src/intuitive_sc/utils/paths.py
@@ -7,3 +7,8 @@ DATA_PATH = os.path.join(ROOT_PATH, "data")
 MODEL_PATH = os.path.join(ROOT_PATH, "models")
 RESULTS_PATH = os.path.join(ROOT_PATH, "results")
 # TODO add default model checkpoint path
+
+INPUT_TRAIN_PATH = os.path.join(
+    DATA_PATH, "combo", "uspto_cjhif_combo_train_reorder.csv"
+)
+INPUT_TEST_PATH = os.path.join(DATA_PATH, "combo", "uspto_cjhif_combo_test_reorder.csv")
