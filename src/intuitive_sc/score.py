05887ce1d96d2fdf2cac5ec6493b137120837d4b
rneeser <rebecca.neeser@epfl.ch>
2023-07-13 18:16:12 +0200

    debug: scoring mode debugged

--- src/intuitive_sc/score.py
+++ src/intuitive_sc/score.py
@@ -1,4 +1,5 @@
 import argparse
+import multiprocessing
 import os
 from typing import Callable, List, Optional
 
@@ -9,14 +10,10 @@ from pytorch_lightning import Trainer
 from rdkit import Chem
 
 from intuitive_sc.data.datamodule import CustomDataModule
-from intuitive_sc.data.featurizer import (
-    AVAILABLE_FEATURIZERS,
-    Featurizer,
-    get_featurizer,
-)
-from intuitive_sc.models.gnn import AVAILABLE_GRAPH_ENCODERS
+from intuitive_sc.data.featurizer import AVAILABLE_FEATURIZERS, Featurizer
 from intuitive_sc.models.ranknet import LitRankNet
 from intuitive_sc.utils.logging import get_logger
+from intuitive_sc.utils.paths import DATA_PATH, RESULTS_PATH  # TODO default modelpath
 
 LOGGER = get_logger(__name__)
 
@@ -29,13 +26,23 @@ class Scorer:
         num_workers: Optional[int] = None,
         mc_dropout_samples: int = 1,
         verbose: bool = False,
+        batch_size: int = 32,
+        graph_datapath: str = None,
     ) -> None:
         self.model = model
         self.featurizer = featurizer
+        if num_workers is None:
+            num_workers = multiprocessing.cpu_count() // 2
         self.num_workers = num_workers
         self.model.mc_dropout_samples = mc_dropout_samples
+        self.batch_size = batch_size
+        self.graph_datapath = None if self.model._hparams.fp else graph_datapath
+        self.depth_edges = self.model._hparams.arrange.count("L") - 1
+        if self.model._hparams.arrange[-1] != "L":
+            self.depth_edges += 1
 
         self.trainer = Trainer(
+            precision="16-mixed",
             accelerator="auto",
             devices=1,
             max_epochs=-1,
@@ -46,7 +53,6 @@ class Scorer:
     def score(
         self,
         smiles: List[str],
-        batch_size: int = 32,
         read_fn: Callable = Chem.MolFromSmiles,
     ) -> np.ndarray[float]:
         """
@@ -54,6 +60,7 @@ class Scorer:
 
         Args:
             smiles: list of SMILES string
+            read_fn: function to read SMILES string into a Mol object
 
         Returns:
             np.ndarray of scores
@@ -61,9 +68,13 @@ class Scorer:
         dm = CustomDataModule(
             smiles=smiles,
             featurizer=self.featurizer,
-            batch_size=batch_size,
+            batch_size=self.batch_size,
             num_workers=self.num_workers,
             read_fn=read_fn,
+            use_fp=self.model._hparams.fp,
+            use_geom=self.model._hparams.use_geom,
+            graph_datapath=self.graph_datapath,
+            depth_edges=self.depth_edges,
         )
 
         preds = self.trainer.predict(self.model, dm)
@@ -83,7 +94,7 @@ if __name__ == "__main__":
         prog="intuitive_sc score",
     )
     parser.add_argument(
-        # TODO add a default path
+        # TODO add a default path (import from paths)
         "--model_path",
         type=str,
         help="Path to the model checkpoint",
@@ -114,12 +125,6 @@ if __name__ == "__main__":
         default="graph_2D",
     )
     parser.add_argument(
-        "--graph_encoder",
-        choices=list(AVAILABLE_GRAPH_ENCODERS.keys()),
-        help="Name of the graph encoder to use",
-        default="GCN",
-    )
-    parser.add_argument(
         "--verbose",
         "-v",
         action="store_true",
@@ -127,36 +132,53 @@ if __name__ == "__main__":
         default=False,
     )
     parser.add_argument(
-        "--use_fp",
-        action="store_true",
-        help="Whether to use fingerprints as input",
-        default=False,
+        "--batch_size",
+        type=int,
+        help="Batch size",
+        default=32,
+    )
+    parser.add_argument(
+        "--num_workers",
+        type=int,
+        help="Number of workers for data loading",
+        default=None,
     )
     args = parser.parse_args()
+
+    torch.set_float32_matmul_precision("medium")
 
     # load data
     df = pd.read_csv(args.data_path)
     LOGGER.info(f"Loaded {len(df)} SMILES from {args.data_path}")
     smiles = df[args.compound_col].tolist()
 
-    featurizer = get_featurizer(args.featurizer)
-
     # load model
     model = LitRankNet.load_from_checkpoint(
         args.model_path,
-        input_size=featurizer.dim(),
-        fp=args.use_fp,
     )
 
+    input_base = os.path.basename(args.data_path).split(".")[0]
+    graph_datapath = os.path.join(DATA_PATH, f"{input_base}_graphs_score.pt")
+
     # score
-    scorer = Scorer(model=model, featurizer=featurizer, verbose=args.verbose)
+    scorer = Scorer(
+        model=model,
+        featurizer=args.featurizer,
+        verbose=args.verbose,
+        batch_size=args.batch_size,
+        graph_datapath=graph_datapath,
+    )
     LOGGER.info(f"Scoring {len(smiles)} SMILES")
     scores = scorer.score(smiles=smiles)
+    df["score"] = scores
 
     # save
     if args.save_filepath is None:
-        args.save_filepath = os.path.dirname(args.model_path)
-    os.makedirs(args.save_filepath, exist_ok=True)
+        model_base = os.path.basename(os.path.dirname(os.path.dirname(args.model_path)))
+        args.save_filepath = os.path.join(
+            RESULTS_PATH, f"{input_base}_{model_base}_scores.csv"
+        )
+
+    os.makedirs(os.path.dirname(args.save_filepath), exist_ok=True)
     LOGGER.info(f"Saving scores to {args.save_filepath}")
-    df["score"] = scores
-    df.to_csv(os.path.join(args.save_filepath, "scores.csv"), index=False)
+    df.to_csv(args.save_filepath, index=False)
