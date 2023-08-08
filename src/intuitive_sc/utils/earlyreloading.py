d2d3605ff5557def2fdc462dab4a4bb308cc2eb7
rneeser <rebecca.neeser@epfl.ch>
2023-08-08 15:33:30 +0200

    fix: debug early reloading - removing inheritance

--- src/intuitive_sc/utils/earlyreloading.py
+++ src/intuitive_sc/utils/earlyreloading.py
@@ -14,30 +14,28 @@ class DataLoaderReloader(EarlyStopping):
         self.reload_every_n_epochs = reload_every_n_epochs
         self.num_reloads = 0
         self.epochs_wo_reload = 0
+        self.final_subset = False
 
     def on_validation_end(
         self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
     ) -> None:
-        super().on_validation_end(trainer, pl_module)
         if self._check_on_train_epoch_end or self._should_skip_check(trainer):
             return
 
         if self.reload_every_n_epochs > 0:
-            # Check if the final subset is reached and stop the training
-            # TODO check!
             num_fracs = trainer.max_epochs // self.reload_every_n_epochs
-            final_subset = False
             if num_fracs == self.num_reloads + 1:
-                final_subset = True
+                self.final_subset = True
 
             # Check if the validation threshold is reached
-            self._run_early_stopping_check_adapt(trainer, final_subset)
+            self._run_early_stopping_check_adapt(trainer)
 
         else:
             return
 
     def _run_early_stopping_check_adapt(
-        self, trainer: "pl.Trainer", final_subset: bool
+        self,
+        trainer: "pl.Trainer",
     ) -> None:
         """Checks whether the early stopping condition is met and if so \
             tells the trainer to stop the training."""
@@ -55,12 +53,12 @@ class DataLoaderReloader(EarlyStopping):
         # stop every ddp process if any world process decides to stop
         should_stop = trainer.strategy.reduce_boolean_decision(should_stop, all=False)
 
-        if final_subset:
+        if self.final_subset:
             trainer.should_stop = trainer.should_stop or should_stop
-        if should_stop and final_subset:
+        if should_stop and self.final_subset:
             LOGGER.info("Stopping training on final subset.")
             self.stopped_epoch = trainer.current_epoch
-        elif should_stop and not final_subset:
+        elif should_stop and not self.final_subset:
             LOGGER.info("Validation threshold reached. Early reloading the dataloader.")
             self.num_reloads += 1
             self.epochs_wo_reload = 0
@@ -72,7 +70,7 @@ class DataLoaderReloader(EarlyStopping):
         elif (
             not should_stop and self.epochs_wo_reload == self.reload_every_n_epochs - 1
         ):
-            if final_subset:
+            if self.final_subset:
                 LOGGER.info("Stopping training on final subset.")
                 self.stopped_epoch = trainer.current_epoch
             else:
