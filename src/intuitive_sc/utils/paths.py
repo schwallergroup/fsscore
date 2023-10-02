a08832bfe811a1bcfa38d665b27db269cc4f55a7
rneeser <rebecca.neeser@epfl.ch>
2023-10-02 10:26:13 +0200

    clean repo and update paths

--- src/intuitive_sc/utils/paths.py
+++ src/intuitive_sc/utils/paths.py
@@ -9,13 +9,8 @@ RESULTS_PATH = os.path.join(ROOT_PATH, "results")
 PROCESSED_PATH = os.path.join(DATA_PATH, "processed")
 BEST_MODEL_PATH = os.path.join(
     MODEL_PATH,
-    "combo_train_graph_GGLGGL_ep25x10_2023-09-16",
-    "checkpoints",
-    "ranknet-epoch=242-best_val_loss.ckpt",
+    "pretrain_graph_GGLGGL_ep242_best_valloss",
 )
-# TODO add default model checkpoint path
 
-INPUT_TRAIN_PATH = os.path.join(
-    DATA_PATH, "combo", "uspto_cjhif_combo_train_reorder.csv"
-)
-INPUT_TEST_PATH = os.path.join(DATA_PATH, "combo", "uspto_cjhif_combo_test_reorder.csv")
+INPUT_TRAIN_PATH = os.path.join(DATA_PATH, "uspto_cjhif_combo_train_reorder.csv")
+INPUT_TEST_PATH = os.path.join(DATA_PATH, "uspto_cjhif_combo_test_reorder.csv")
