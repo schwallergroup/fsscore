fix: precision set when using CPU only

src/fsscore/finetuning.py
@@ -79,7 +79,7 @@ class TrackImprovementToPretain(pl.Callback):
             depth_edges=self.depth_edges,
         )
         old_trainer = pl.Trainer(
-            precision="16-mixed",
+            precision="16-mixed" if torch.cuda.is_available() else 16,
             accelerator="auto",
             strategy="ddp",
             devices=torch.cuda.device_count(),
@@ -242,7 +242,7 @@ def finetune(
 
     # fine-tuning scheduler
     trainer = pl.Trainer(
-        precision="16-mixed",
+        precision="16-mixed" if torch.cuda.is_available() else 16,
         accelerator="auto",
         strategy="ddp",
         devices=torch.cuda.device_count(),

src/fsscore/models/nn_utils.py
@@ -98,7 +98,7 @@ def get_new_model_and_trainer(
         arrange=arrange,
     )
     trainer = pl.Trainer(
-        precision="16-mixed",
+        precision="16-mixed" if torch.cuda.is_available() else 16,
         logger=logger,
         accelerator="auto",
         devices=torch.cuda.device_count(),

src/fsscore/score.py
@@ -47,7 +47,7 @@ class Scorer:
             self.depth_edges += 1
 
         self.trainer = Trainer(
-            precision="16-mixed",
+            precision="16-mixed" if torch.cuda.is_available() else 16,
             accelerator="auto",
             devices=1,
             max_epochs=-1,

src/fsscore/test.py
@@ -5,6 +5,7 @@ from typing import List
 import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
+import torch
 from pytorch_lightning.loggers import WandbLogger
 
 from fsscore.data.datamodule import CustomDataModule
@@ -117,7 +118,7 @@ if __name__ == "__main__":
 
     LOGGER.info("Testing model")
     trainer = pl.Trainer(
-        precision="16-mixed",
+        precision="16-mixed" if torch.cuda.is_available() else 16,
         accelerator="auto",
         devices=1,
         max_epochs=-1,